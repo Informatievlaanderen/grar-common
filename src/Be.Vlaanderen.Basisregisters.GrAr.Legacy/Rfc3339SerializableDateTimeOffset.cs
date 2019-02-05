20ac089ebae786c32c3275c18125d006f82b443e
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-02-05 10:33:23 +0100

    fix: create converter for serialization issues with rfcdatetimeoffset

--- src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/Rfc3339SerializableDateTimeOffset.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/Rfc3339SerializableDateTimeOffset.cs
@@ -1,5 +1,6 @@
 namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy
 {
+    using Newtonsoft.Json;
     using System;
     using System.Globalization;
     using System.Xml;
@@ -122,6 +123,40 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy
 
                 result = null;
                 return false;
+            }
+        }
+    }
+
+    public class Rfc3339SerializableDateTimeOffsetConverter : JsonConverter<Rfc3339SerializableDateTimeOffset>
+    {
+        public override void WriteJson(JsonWriter writer,
+            Rfc3339SerializableDateTimeOffset value,
+            JsonSerializer serializer)
+        {
+            var dateTimeOffset = (DateTimeOffset)value;
+
+            serializer.Serialize(writer, dateTimeOffset);
+        }
+
+        public override Rfc3339SerializableDateTimeOffset ReadJson(JsonReader reader,
+            Type objectType,
+            Rfc3339SerializableDateTimeOffset existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            switch (serializer.DateParseHandling)
+            {
+                case DateParseHandling.None:
+                    var parsedDateTimeOffset = DateTimeOffset.Parse((string)reader.Value);
+                    return new Rfc3339SerializableDateTimeOffset(parsedDateTimeOffset);
+                case DateParseHandling.DateTime:
+                    var dateTime = new DateTimeOffset((DateTime)reader.Value);
+                    return new Rfc3339SerializableDateTimeOffset(dateTime);
+                case DateParseHandling.DateTimeOffset:
+                    var dateTimeOffset = (DateTimeOffset)reader.Value;
+                    return new Rfc3339SerializableDateTimeOffset(dateTimeOffset);
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
     }
