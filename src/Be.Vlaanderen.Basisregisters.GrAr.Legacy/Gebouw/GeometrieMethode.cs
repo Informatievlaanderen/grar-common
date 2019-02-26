0198be2f0617adadd24cfbfc701395cea516519f
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-02-26 13:11:03 +0100

    fix: add attributes to enums

--- src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/Gebouw/GeometrieMethode.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/Gebouw/GeometrieMethode.cs
@@ -1,23 +1,29 @@
 namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.Gebouw
 {
+    using System.Runtime.Serialization;
+
     /// <summary>
     /// De methode waarop de geometrie is verworven.
     /// </summary>
+    [DataContract(Name = "GeometrieMethode", Namespace = "")]
     public enum GeometrieMethode
     {
         /// <summary>
         /// Het object is opgemeten volgens de GRB specificaties.
         /// </summary>
+        [EnumMember]
         IngemetenGRB = 1,
 
         /// <summary>
         /// Het object is geschetst.
         /// </summary>
+        [EnumMember]
         Ingeschetst = 2,
 
         /// <summary>
         /// Het object is opgemeten (genomen van het as-built plan of andere dataset).
         /// </summary>
+        [EnumMember]
         Ingemeten = 3
     }
 }
--- src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/Gebouweenheid/GebouweenheidStatus.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/Gebouweenheid/GebouweenheidStatus.cs
@@ -1,28 +1,35 @@
 namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.Gebouweenheid
 {
+    using System.Runtime.Serialization;
+
     /// <summary>
     /// De status van een gebouweenheid.
     /// </summary>
+    [DataContract(Name = "GebouweenheidStatus", Namespace = "")]
     public enum GebouweenheidStatus
     {
         /// <summary>
         /// Een bouwaanvraag is toegekend voor de gebouweenheid.
         /// </summary>
+        [EnumMember]
         Gepland = 1,
 
         /// <summary>
         /// De gebouweenheid is gerealiseerd (werk afgerond) en is observeerbaar.
         /// </summary>
+        [EnumMember]
         Gerealiseerd = 2,
 
         /// <summary>
         /// De gebouweenheid is gesloopt, gesplitst of samengevoegd.
         /// </summary>
+        [EnumMember]
         Gehistoreerd = 3,
 
         /// <summary>
         /// De bouwaanvraag voor de gebouweenheid is niet toegekend, geannuleerd of verlopen.
         /// </summary>
+        [EnumMember]
         NietGerealiseerd = 4
     }
 }
