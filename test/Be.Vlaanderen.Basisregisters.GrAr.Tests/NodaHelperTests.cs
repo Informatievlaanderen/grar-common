f2ef53e9ed4662a0269ae981dc11335c8c7a1758
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-02-28 12:31:55 +0100

    test: corrected belgiandatetimeoffset test

--- test/Be.Vlaanderen.Basisregisters.GrAr.Tests/NodaHelperTests.cs
+++ test/Be.Vlaanderen.Basisregisters.GrAr.Tests/NodaHelperTests.cs
@@ -1,9 +1,9 @@
 namespace Be.Vlaanderen.Basisregisters.GrAr.Tests
 {
-    using System;
     using Common;
     using FluentAssertions;
     using NodaTime.Extensions;
+    using System;
     using Xunit;
 
     public class NodaHelperTests
@@ -11,12 +11,12 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Tests
         [Fact]
         public void WhenConvertingBelgianDateTime_GivenDateTime_ThenOffset_is_set()
         {
-            var dateTime = new DateTime(2019, 1, 1, 23, 0, 0);
+            var dateTime = new DateTime(2019, 1, 1, 23, 0, 0, DateTimeKind.Utc);
             var utcDateTime = new DateTimeOffset(dateTime);
             var instantDate = utcDateTime.ToInstant();
 
             var expectedOffset = 1;
-            instantDate.ToBelgianDateTimeOffset().Should().Be(new DateTimeOffset(dateTime, TimeSpan.FromHours(expectedOffset)));
+            instantDate.ToBelgianDateTimeOffset().Should().Be(new DateTimeOffset(2019, 1, 2, 0, 0, 0, TimeSpan.FromHours(expectedOffset)));
         }
     }
 }
