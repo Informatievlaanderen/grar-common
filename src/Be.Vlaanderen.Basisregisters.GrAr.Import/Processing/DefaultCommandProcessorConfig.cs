2c70a4d2718fb56cb88f9783bb77361f057e9a1a
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-08-16 13:37:59 +0200

    feat: add setting to wait for user input or not
    BREAKING CHANGE: Add WaitForUserInput to ICommandProcessorConfig

--- src/Be.Vlaanderen.Basisregisters.GrAr.Import/Processing/DefaultCommandProcessorConfig.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Import/Processing/DefaultCommandProcessorConfig.cs
@@ -8,6 +8,7 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Import.Processing
         public int BufferSize { get; set; } = 100;
         public int NrOfConsumers { get; set; } = 4;
         public int BatchSize { get; set; } = 500;
+        public bool WaitForUserInput { get; } = false;
 
         public override string ToString() => $"{Environment.NewLine}" +
                                              $"NrOfProducers: {NrOfProducers}{Environment.NewLine}" +
