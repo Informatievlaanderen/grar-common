2d42182373651127c33dcb218e90bdab242a89e0
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-11-28 15:09:05 +0100

    feat: add datetimeoffset extensions for extracts

---
+++ test/Be.Vlaanderen.Basisregisters.GrAr.Tests/DbaseStringExtensionsTests.cs
@@ -0,0 +1,32 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Tests
+{
+    using System;
+    using Extracts;
+    using FluentAssertions;
+    using Shaperon;
+    using Xunit;
+
+    public class DbaseStringExtensionsTests
+    {
+        [Fact]
+        public void WhenFromDateTime_GivenDateTimeOffset_ThenShouldBeExpectedString()
+        {
+            var expected = "2019-11-28T14:55:30+01:00";
+            var dateTimeOffset = new DateTimeOffset(2019, 11, 28, 14, 55, 30, TimeSpan.FromHours(1));
+
+            dateTimeOffset.FromDateTimeOffset().Should().Be(expected);
+        }
+
+        [Fact]
+        public void WhenSettingValue_GivenDateTimeOffset_ThenShouldBeExpectedDbaseString()
+        {
+            var expected = "2019-11-28T14:55:30+01:00";
+            var dateTimeOffset = new DateTimeOffset(2019, 11, 28, 14, 55, 30, TimeSpan.FromHours(1));
+
+            var dbaseString = new DbaseString(DbaseField.CreateStringField(new DbaseFieldName("test"), new DbaseFieldLength(25)));
+            dbaseString.SetValue(dateTimeOffset);
+
+            dbaseString.Value.Should().Be(expected);
+        }
+    }
+}
