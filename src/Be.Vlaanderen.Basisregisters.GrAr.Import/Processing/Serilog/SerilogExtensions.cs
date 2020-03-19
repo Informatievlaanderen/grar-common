89065d0a1f2400145fb4f2d41f3eb16456cbd9f7
David Cumps <david@cumps.be>
2020-03-19 18:12:12 +0100

    style: readable arguments

--- src/Be.Vlaanderen.Basisregisters.GrAr.Import/Processing/Serilog/SerilogExtensions.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Import/Processing/Serilog/SerilogExtensions.cs
@@ -7,24 +7,29 @@ namespace Serilog
 {
     public static class LoggerConfigurationExtensions
     {
-        public static LoggerConfiguration SendGridSmtp(this LoggerSinkConfiguration sinkConfiguration, string apiKey, string subject, string fromEmail, string toEmail, LogEventLevel restrictedToMinimumLevel, string smtpServer = "smtp.sendgrid.net", int port = 465, bool enableSsl = true)
+        public static LoggerConfiguration SendGridSmtp(
+            this LoggerSinkConfiguration sinkConfiguration,
+            string apiKey,
+            string subject, 
+            string fromEmail,
+            string toEmail,
+            LogEventLevel restrictedToMinimumLevel, 
+            string smtpServer = "smtp.sendgrid.net",
+            int port = 465, 
+            bool enableSsl = true)
         {
-            return
-                sinkConfiguration
-                    .Email
-                    (
-                        new EmailConnectionInfo
-                        {
-                            MailServer = smtpServer,
-                            NetworkCredentials = new NetworkCredential(userName: "apikey", password: apiKey),
-                            EmailSubject = subject,
-                            FromEmail = fromEmail,
-                            ToEmail = toEmail,
-                            EnableSsl = enableSsl,
-                            Port = port
-                        },
-                        restrictedToMinimumLevel: restrictedToMinimumLevel
-                    );
+            return sinkConfiguration.Email(
+                new EmailConnectionInfo
+                {
+                    MailServer = smtpServer,
+                    NetworkCredentials = new NetworkCredential(userName: "apikey", password: apiKey),
+                    EmailSubject = subject,
+                    FromEmail = fromEmail,
+                    ToEmail = toEmail,
+                    EnableSsl = enableSsl,
+                    Port = port
+                },
+                restrictedToMinimumLevel: restrictedToMinimumLevel);
         }
     }
 }
