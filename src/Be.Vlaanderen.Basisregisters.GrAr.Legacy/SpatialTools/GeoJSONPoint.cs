a1038d5fd21048ef44f6ba1d30f902bafd1cb99c
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-04-28 11:48:41 +0200

    fix: add documentation for GeoJson objects GRAR-412

--- src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/SpatialTools/GeoJSONPoint.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/SpatialTools/GeoJSONPoint.cs
@@ -7,8 +7,18 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.SpatialTools
     /// </summary>
     public class GeoJSONPoint
     {
+        /// <summary>
+        /// Coördinaten volgens Lambert-72 (EPSG:31370).
+        /// </summary>
         [JsonConverter(typeof(PointCoordinatesConverter))]
+        [JsonProperty(Required = Required.DisallowNull)]
         public double[] Coordinates { get; set; }
+
+
+        /// <summary>
+        /// GeoJSON-geometrietype.
+        /// </summary>
+        [JsonProperty(Required = Required.DisallowNull)]
         public string Type { get; set; }
 
         public GeoJSONPoint()
--- src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/SpatialTools/GeoJSONPolygon.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/SpatialTools/GeoJSONPolygon.cs
@@ -11,11 +11,13 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.SpatialTools
         /// Coördinaten volgens Lambert-72 (EPSG:31370).
         /// </summary>
         [JsonConverter(typeof(PolygonCoordinatesConverter))]
+        [JsonProperty(Required = Required.DisallowNull)]
         public double[][][] Coordinates { get; set; }
 
         /// <summary>
         /// GeoJSON-geometrietype.
         /// </summary>
+        [JsonProperty(Required = Required.DisallowNull)]
         public string Type { get; set; }
 
         public GeoJSONPolygon()
