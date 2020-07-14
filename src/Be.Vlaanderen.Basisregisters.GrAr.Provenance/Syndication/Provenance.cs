46c2093885e30e7b94d35cd4c09f2fe3d08d70a9
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-07-14 08:10:05 +0200

    feat: add timestamp to syndication provenance GRAR-1451
    BREAKING CHANGE: Add timestamp (Instant or DateTimeOffset) to ctor of provenance syndication

--- src/Be.Vlaanderen.Basisregisters.GrAr.Provenance/Syndication/Provenance.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Provenance/Syndication/Provenance.cs
@@ -1,24 +1,44 @@
 namespace Be.Vlaanderen.Basisregisters.GrAr.Provenance.Syndication
 {
+    using System;
     using System.Runtime.Serialization;
+    using Common;
     using GrAr.Provenance;
     using Newtonsoft.Json;
+    using NodaTime;
+    using Utilities;
 
     [DataContract(Name = "Creatie", Namespace = "")]
     public class Provenance
     {
-        [DataMember(Name = "Organisatie", Order = 0)]
+        [DataMember(Name = "Tijdstip", Order = 0)]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public string Timestamp { get; set; }
+
+        [DataMember(Name = "Organisatie", Order = 1)]
         [JsonProperty(Required = Required.DisallowNull)]
         public string Organisation { get; set; }
 
-        [DataMember(Name = "Reden", Order = 1)]
+        [DataMember(Name = "Reden", Order = 2)]
         [JsonProperty(Required = Required.DisallowNull)]
         public string Reason { get; set; }
 
         public Provenance(
+            Instant timestamp,
+            Organisation? organisation,
+            Reason reason)
+            : this (
+                timestamp.ToBelgianDateTimeOffset(),
+                organisation,
+                reason)
+        { }
+
+        public Provenance(
+            DateTimeOffset timestamp,
             Organisation? organisation,
             Reason reason)
         {
+            Timestamp = new Rfc3339SerializableDateTimeOffset(timestamp).ToString();
             Organisation = organisation?.ToName();
             Reason = reason;
         }
