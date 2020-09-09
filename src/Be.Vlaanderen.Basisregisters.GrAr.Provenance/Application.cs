daee546b479ded5875323a93a2fe7322c52b3c51
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-09-09 09:39:32 +0200

    feat: add registers as application provenance

--- src/Be.Vlaanderen.Basisregisters.GrAr.Provenance/Application.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Provenance/Application.cs
@@ -7,6 +7,12 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Provenance
         GrbCrabMatching = 2,
         CrabWstEditService = 3,
         Lara = 4,
-        BPost = 5
+        BPost = 5,
+        MunicipalityRegistry = 6,
+        PostalRegistry = 7,
+        StreetNameRegistry = 8,
+        AddressRegistry = 9,
+        BuildingRegistry = 10,
+        ParcelRegistry = 11
     }
 }
