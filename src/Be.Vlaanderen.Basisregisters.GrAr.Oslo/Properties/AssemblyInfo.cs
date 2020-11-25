baf75f228f0ef4a67cba50f0d9a93ea15dcac8ec
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-11-25 15:11:43 +0100

    chore: add projects

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Oslo/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Basisregisters Vlaanderen")]
+[assembly: AssemblyDescription("GrAr Common Oslo")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("82922787-e967-4d37-94b0-67b193191cfe")]
