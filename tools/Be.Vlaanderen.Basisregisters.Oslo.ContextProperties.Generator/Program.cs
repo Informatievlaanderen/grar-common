bacbeb16bd199937111eea66bdae930a6682d337
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-11-25 19:25:33 +0100

    feat: fetch jsonld context from source

--- tools/Be.Vlaanderen.Basisregisters.Oslo.ContextProperties.Generator/Program.cs
+++ tools/Be.Vlaanderen.Basisregisters.Oslo.ContextProperties.Generator/Program.cs
@@ -4,6 +4,7 @@ namespace Be.Vlaanderen.Basisregisters.Oslo.ContextProperties.Generator
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Extensions.Configuration;
 
@@ -25,20 +26,28 @@ namespace Be.Vlaanderen.Basisregisters.Oslo.ContextProperties.Generator
 
         static async Task Main(string[] args)
         {
+            var cancellationTokenSource = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, cancelArgs) => cancellationTokenSource.Cancel();
+
             var directory = ContextPropertiesDirectory.CreateVersionDirectory();
             foreach (var contextInfo in GetContexts())
-                await CreateContextPropertiesFile(directory, contextInfo);
+                await CreateContextPropertiesFile(directory, contextInfo, cancellationTokenSource.Token);
         }
 
-        private static async Task CreateContextPropertiesFile(DirectoryInfo path, ContextInformation contextInfo)
+        private static async Task CreateContextPropertiesFile(
+            DirectoryInfo directory,
+            ContextInformation contextInfo,
+            CancellationToken cancellationToken)
         {
-            //todo: implement and extract!
+            var context = await new ContextSource(contextInfo.SourceUrl).Fetch(cancellationToken);
 
-            // fetch context from contextInfo.SourceUrl
+            var fileName = Path.Combine(directory.FullName, contextInfo.FileName + ".json"); // dump json for now
+            await File.WriteAllTextAsync(fileName, context.ToString(), cancellationToken);
+
             // ...maybe there needs to happen something here too
+
             // write cs file
             // profit!
-            throw new NotImplementedException();
         }
 
         private static IEnumerable<ContextInformation> GetContexts()
