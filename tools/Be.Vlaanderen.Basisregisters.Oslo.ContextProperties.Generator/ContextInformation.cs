985e8b4ca6e4091ba13a4ef0cccc650af63bdd2d
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-11-27 10:56:02 +0100

    style: apply review style comments

--- tools/Be.Vlaanderen.Basisregisters.Oslo.ContextProperties.Generator/ContextInformation.cs
+++ tools/Be.Vlaanderen.Basisregisters.Oslo.ContextProperties.Generator/ContextInformation.cs
@@ -4,13 +4,13 @@ namespace Be.Vlaanderen.Basisregisters.Oslo.ContextProperties.Generator
 
     public class ContextInformation
     {
+        public string Name { get; }
+        public Uri SourceUrl { get; }
+
         public ContextInformation(string name, Uri sourceUrl)
         {
             Name = name;
             SourceUrl = sourceUrl;
         }
-
-        public string Name { get; }
-        public Uri SourceUrl { get; }
     }
 }
--- tools/Be.Vlaanderen.Basisregisters.Oslo.ContextProperties.Generator/Program.cs
+++ tools/Be.Vlaanderen.Basisregisters.Oslo.ContextProperties.Generator/Program.cs
@@ -8,7 +8,7 @@ namespace Be.Vlaanderen.Basisregisters.Oslo.ContextProperties.Generator
     using System.Threading.Tasks;
     using Microsoft.Extensions.Configuration;
 
-    class Program
+    public class Program
     {
         private static readonly IConfiguration Configuration;
 
