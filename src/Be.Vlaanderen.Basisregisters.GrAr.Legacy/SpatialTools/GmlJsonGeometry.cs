332681c1525fac526a8ba3ad61bf275ca5f7232c
Yusuf <yusuf.mavzer1@gmail.com>
2021-12-13 12:07:34 +0100

    feat: add GmlJsonGeometry with Point child

--- src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/SpatialTools/GmlJsonGeometry.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/SpatialTools/GmlJsonGeometry.cs
@@ -3,26 +3,26 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.SpatialTools
     using Newtonsoft.Json;
 
     /// <summary>
-    /// Een GeoJSON punt.
+    /// Een geometrie met GML3.
     /// </summary>
-    public class GeoJSONPoint
+    public class GmlJsonGeometry
     {
         /// <summary>
-        /// Coördinaten volgens Lambert-72 (EPSG:31370).
-        /// </summary>
-        [JsonConverter(typeof(PointCoordinatesConverter))]
-        [JsonProperty(Required = Required.DisallowNull)]
-        public double[] Coordinates { get; set; }
-        
-        /// <summary>
-        /// GeoJSON-geometrietype.
+        /// Geometrietype.
         /// </summary>
         [JsonProperty(Required = Required.DisallowNull)]
         public string Type { get; set; }
 
-        public GeoJSONPoint()
+        /// <summary>
+        /// GML3: geometrie object serializatie.
+        /// </summary>
+        [JsonProperty(Required = Required.DisallowNull)]
+        public string Gml { get; set; }
+
+        public GmlJsonGeometry(string type, string gml)
         {
-            Type = "Point";
+            Type = type;
+            Gml = gml;
         }
     }
 }
