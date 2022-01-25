da3c88eaf250f1c813c9e740b77b2340d064a8da
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-01-25 16:06:06 +0100

    fix: add Provenance ctor

--- src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/Common/Provenance.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/Common/Provenance.cs
@@ -2,35 +2,30 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.Common
 {
     public class Provenance
     {
-
         /// <summary>
         /// Format ISO-8601
         /// </summary>
         public string Timestamp { get; }
 
-        /// <summary>
-        ///
-        /// </summary>
         public string Application { get; }
-
-        /// <summary>
-        ///
-        /// </summary>
         public string Modification { get; }
-
-        /// <summary>
-        ///
-        /// </summary>
         public string Operator { get; }
-
-        /// <summary>
-        ///
-        /// </summary>
         public string Organisation { get; }
-
-        /// <summary>
-        ///
-        /// </summary>
         public string Reason { get; }
+
+        public Provenance(string timestamp,
+            string application,
+            string modification,
+            string @operator,
+            string organisation,
+            string reason)
+        {
+            Timestamp = timestamp;
+            Application = application;
+            Modification = modification;
+            Operator = @operator;
+            Organisation = organisation;
+            Reason = reason;
+        }
     }
 }
