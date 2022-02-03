4ed97f576659ae213d412809fe2d82372f3c4cc0
Arne Dumarey <dumarey.arne@gmail.com>
2022-02-03 15:00:33 +0100

    fix: remove operator from provenance contract

--- src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/Common/Provenance.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/Common/Provenance.cs
@@ -6,24 +6,20 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.Common
         /// Format ISO-8601
         /// </summary>
         public string Timestamp { get; }
-
         public string Application { get; }
         public string Modification { get; }
-        public string Operator { get; }
         public string Organisation { get; }
         public string Reason { get; }
 
         public Provenance(string timestamp,
             string application,
             string modification,
-            string @operator,
             string organisation,
             string reason)
         {
             Timestamp = timestamp;
             Application = application;
             Modification = modification;
-            Operator = @operator;
             Organisation = organisation;
             Reason = reason;
         }
