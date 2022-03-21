c71b2d431927065098b93951f5ca5163fa6a7d82
pgallik <patrickgallik@gmail.com>
2022-03-21 14:17:50 +0100

    fix: add 3 new streetname events

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/StreetNameRegistry/StreetNameWasMigratedToMunicipality.cs
@@ -0,0 +1,61 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.StreetNameRegistry
+{
+    using System.Collections.Generic;
+    using Common;
+    using Contracts;
+
+    public class StreetNameWasMigratedToMunicipality : IQueueMessage
+    {
+        public string MunicipalityId { get; }
+
+        public string NisCode { get; }
+
+        public string StreetNameId { get; }
+
+        public int PersistentLocalId { get; }
+
+        public string Status { get; }
+
+        public string? PrimaryLanguage { get; }
+
+        public string? SecondaryLanguage { get; }
+
+        public IDictionary<string, string> Names { get; }
+
+        public IDictionary<string, string> HomonymAdditions { get; }
+
+        public bool IsCompleted { get; }
+
+        public bool IsRemoved { get; }
+
+        public Provenance Provenance { get;  }
+
+        public StreetNameWasMigratedToMunicipality(
+            string municipalityId,
+            string nisCode,
+            string streetNameId,
+            int persistentLocalId,
+            string status,
+            string? primaryLanguage,
+            string? secondaryLanguage,
+            IDictionary<string, string> names,
+            IDictionary<string, string> homonymAdditions,
+            bool isCompleted,
+            bool isRemoved,
+            Provenance provenance)
+        {
+            MunicipalityId = municipalityId;
+            NisCode = nisCode;
+            StreetNameId = streetNameId;
+            PersistentLocalId = persistentLocalId;
+            Status = status;
+            PrimaryLanguage = primaryLanguage;
+            SecondaryLanguage = secondaryLanguage;
+            Names = names;
+            HomonymAdditions = homonymAdditions;
+            IsCompleted = isCompleted;
+            IsRemoved = isRemoved;
+            Provenance = provenance;
+        }
+    }
+}
---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/StreetNameRegistry/StreetNameWasProposedV2.cs
@@ -0,0 +1,32 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.StreetNameRegistry
+{
+    using System.Collections.Generic;
+    using Common;
+    using Contracts;
+
+    public class StreetNameWasProposedV2 : IQueueMessage
+    {
+        public string MunicipalityId { get; }
+
+        public string NisCode { get; }
+
+        public IDictionary<string, string> StreetNameNames { get; }
+
+        public int PersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public StreetNameWasProposedV2(string municipalityId,
+            string nisCode,
+            IDictionary<string, string> streetNameNames,
+            int persistentLocalId,
+            Provenance provenance)
+        {
+            MunicipalityId = municipalityId;
+            NisCode = nisCode;
+            StreetNameNames = streetNameNames;
+            PersistentLocalId = persistentLocalId;
+            Provenance = provenance;    
+        }
+    }
+}
