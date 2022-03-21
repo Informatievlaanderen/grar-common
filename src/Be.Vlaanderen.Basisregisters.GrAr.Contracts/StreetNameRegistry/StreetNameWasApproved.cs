8fee5e0843f7286d1e6a001f1d81c843756e2f74
pgallik <patrickgallik@gmail.com>
2022-03-21 14:17:50 +0100

    fix: add 3 new streetname events

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/StreetNameRegistry/StreetNameWasApproved.cs
@@ -0,0 +1,24 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.StreetNameRegistry
+{
+    using Common;
+    using Contracts;
+
+    public class StreetNameWasApproved : IQueueMessage
+    {
+        public string MunicipalityId { get; }
+
+        public int PersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public StreetNameWasApproved(
+            string municipalityId,
+            int persistentLocalId,
+            Provenance provenance)
+        {
+            MunicipalityId = municipalityId;
+            PersistentLocalId = persistentLocalId;
+            Provenance = provenance;    
+        }
+    }
+}
