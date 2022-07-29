fc11b6e613faf20d557aeb7681698c71c9c88eac
jvandaal <jonas.vandaal@outlook.be>
2022-07-29 15:55:48 +0200

    feat: add AddressWasDeregulated

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/AddressRegistry/AddressWasDeregulated.cs
@@ -0,0 +1,22 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry
+{
+    using Common;
+
+    public class AddressWasDeregulated : IQueueMessage
+    {
+        public int StreetNamePersistentLocalId { get; }
+
+        public int AddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public AddressWasDeregulated(int streetNamePersistentLocalId,
+            int addressPersistentLocalId,
+            Provenance provenance)
+        {
+            StreetNamePersistentLocalId = streetNamePersistentLocalId;
+            AddressPersistentLocalId = addressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
