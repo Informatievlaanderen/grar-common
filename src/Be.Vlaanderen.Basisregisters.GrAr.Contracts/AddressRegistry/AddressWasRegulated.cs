7809297044db8b11f05422a7448715d3b4cb0ec9
emalfroy <egon.malfroy@vlaanderen.be>
2022-08-01 09:16:07 +0200

    feat: add address was regulated

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/AddressRegistry/AddressWasRegulated.cs
@@ -0,0 +1,22 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry
+{
+    using Common;
+
+    public class AddressWasRegulated : IQueueMessage
+    {
+        public int StreetNamePersistentLocalId { get; }
+
+        public int AddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public AddressWasRegulated(int streetNamePersistentLocalId,
+            int addressPersistentLocalId,
+            Provenance provenance)
+        {
+            StreetNamePersistentLocalId = streetNamePersistentLocalId;
+            AddressPersistentLocalId = addressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
