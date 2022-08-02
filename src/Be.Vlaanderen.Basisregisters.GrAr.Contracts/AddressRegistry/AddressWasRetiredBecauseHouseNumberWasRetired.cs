9eff3e69944feb479da830458b6b3165a4df0d19
emalfroy <egon.malfroy@vlaanderen.be>
2022-08-02 13:40:34 +0200

    additional retire and reject address events

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/AddressRegistry/AddressWasRetiredBecauseHouseNumberWasRetired.cs
@@ -0,0 +1,23 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry
+{
+    using Common;
+
+    public class AddressWasRetiredBecauseHouseNumberWasRetired : IQueueMessage
+    {
+        public int StreetNamePersistentLocalId { get; }
+
+        public int AddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public AddressWasRetiredBecauseHouseNumberWasRetired(
+            int streetNamePersistentLocalId,
+            int addressPersistentLocalId,
+            Provenance provenance)
+        {
+            StreetNamePersistentLocalId = streetNamePersistentLocalId;
+            AddressPersistentLocalId = addressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/AddressRegistry/AddressWasRetiredV2.cs
@@ -0,0 +1,22 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry
+{
+    using Common;
+
+    public class AddressWasRetiredV2 : IQueueMessage
+    {
+        public int StreetNamePersistentLocalId { get; }
+
+        public int AddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public AddressWasRetiredV2(int streetNamePersistentLocalId,
+            int addressPersistentLocalId,
+            Provenance provenance)
+        {
+            StreetNamePersistentLocalId = streetNamePersistentLocalId;
+            AddressPersistentLocalId = addressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
