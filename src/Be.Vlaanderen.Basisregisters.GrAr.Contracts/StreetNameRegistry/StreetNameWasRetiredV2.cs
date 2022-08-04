523e0d355484b83f244702e7b40d5bcbe29c44b3
emalfroy <egon.malfroy@vlaanderen.be>
2022-08-04 16:26:28 +0200

    add streetname was retired v2

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/StreetNameRegistry/StreetNameWasRetiredV2.cs
@@ -0,0 +1,24 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.StreetNameRegistry
+{
+    using Common;
+    using Contracts;
+
+    public class StreetNameWasRetiredV2 : IQueueMessage
+    {
+        public string MunicipalityId { get; }
+
+        public int PersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public StreetNameWasRetiredV2(
+            string municipalityId,
+            int persistentLocalId,
+            Provenance provenance)
+        {
+            MunicipalityId = municipalityId;
+            PersistentLocalId = persistentLocalId;
+            Provenance = provenance;    
+        }
+    }
+}
