112b4085c0165c391792dfc1aa177d2ed60ca97c
jvandaal <jonas.vandaal@outlook.be>
2022-08-17 08:42:34 +0200

    feat: add PositieSpecificatieMethode

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Edit/Contracts/PositieSpecificatie.cs
@@ -0,0 +1,45 @@
+﻿namespace Be.Vlaanderen.Basisregisters.GrAr.Edit.Contracts;
+
+using System.Runtime.Serialization;
+
+/// <summary>
+/// De specificatiemethode van de positie.
+/// </summary>
+[DataContract(Name = "PositieSpecificatie", Namespace = "")]
+public enum PositieSpecificatie
+{
+    /// <summary>
+    /// De positie duidt een gemeente aan.
+    /// </summary>
+    [EnumMember] Gemeente = 1,
+
+    /// <summary>
+    /// De positie duidt een perceel aan.
+    /// </summary>
+    [EnumMember] Perceel = 3,
+
+    /// <summary>
+    /// De positie duidt een lot aan.
+    /// </summary>
+    [EnumMember] Lot = 4,
+
+    /// <summary>
+    /// De positie duidt een standplaats aan.
+    /// </summary>
+    [EnumMember] Standplaats = 5,
+
+    /// <summary>
+    /// De positie duidt een ligplaats aan.
+    /// </summary>
+    [EnumMember] Ligplaats = 6,
+
+    /// <summary>
+    /// De positie duidt een ingang aan.
+    /// </summary>
+    [EnumMember] Ingang = 9,
+
+    /// <summary>
+    /// De positie duidt een wegsegment aan.
+    /// </summary>
+    [EnumMember] Wegsegment = 11
+}
