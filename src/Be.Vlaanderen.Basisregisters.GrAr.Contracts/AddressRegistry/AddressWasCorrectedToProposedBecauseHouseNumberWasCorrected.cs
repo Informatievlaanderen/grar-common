d01c516fc04f8b5cac02f0d7b48e4560f6133aa7
jvandaal <jonas.vandaal@outlook.be>
2022-09-29 16:29:02 +0200

    feat: add AddressWasCorrectedToProposedV2

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/AddressRegistry/AddressWasCorrectedToProposedBecauseHouseNumberWasCorrected.cs
@@ -0,0 +1,23 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry
+{
+    using Common;
+
+    public class AddressWasCorrectedToProposedV2 : IQueueMessage
+    {
+        public int StreetNamePersistentLocalId { get; }
+
+        public int AddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public AddressWasCorrectedToProposedV2(
+            int streetNamePersistentLocalId,
+            int addressPersistentLocalId,
+            Provenance provenance)
+        {
+            StreetNamePersistentLocalId = streetNamePersistentLocalId;
+            AddressPersistentLocalId = addressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
