700535c0c7e6c0bc7c89aa20ae5c16b713e5ce6b
Arne Dumarey <dumarey.arne@gmail.com>
2022-10-06 11:28:35 +0200

    feat: add postinfodetailgemeente

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/PostInfo/PostinfoDetailGemeente.cs
@@ -0,0 +1,34 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.PostInfo
+{
+    using System.Runtime.Serialization;
+    using Gemeente;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// De gemeente aan dewelke de postinfo is toegewezen.
+    /// </summary>
+    [DataContract(Name = "Gemeente", Namespace = "")]
+    public class PostinfoDetailGemeente
+    {
+        /// <summary>
+        /// De objectidentificator van de gekoppelde gemeente.
+        /// </summary>
+        [DataMember(Name = "ObjectId", Order = 1)]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public string ObjectId { get; set; }
+
+        /// <summary>
+        /// De URL die de details van de meest recente versie van de gekoppelde gemeente weergeeft.
+        /// </summary>
+        [DataMember(Name = "Detail", Order = 2)]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public string Detail { get; set; }
+
+        /// <summary>
+        /// De gemeentenaam in de eerste officiële taal van de gemeente.
+        /// </summary>
+        [DataMember(Name = "Gemeentenaam", Order = 3)]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public Gemeentenaam Gemeentenaam { get; set; }
+    }
+}
