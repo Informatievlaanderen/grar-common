4c3f4af3b77213b5d7b912d3417198631487f0c0
jvandaal <jonas.vandaal@outlook.be>
2022-11-09 15:43:45 +0100

    feat: catch ArgumentException when validating GML polygon

--- test/Be.Vlaanderen.Basisregisters.GrAr.Tests/Edit/ValidatorTests/GmlPolygonValidatorTests.cs
+++ test/Be.Vlaanderen.Basisregisters.GrAr.Tests/Edit/ValidatorTests/GmlPolygonValidatorTests.cs
@@ -27,5 +27,14 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Tests.Edit.ValidatorTests
         {
             GmlPolygonValidator.IsValid(gml, _gmlReader).Should().Be(expectedResult);
         }
+
+        [Fact]
+        public void GivenPolygonWithPointsThatDoNotFormClosedLinestring_ThenGmlIsNotValid()
+        {
+            var gml =
+                "<gml:Polygon srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:exterior><gml:LinearRing><gml:posList>140204.15277253836 186724.74131567031 140291.06016454101 186726.38355567306 140288.22675654292 186838.25798767805 140281.19098053873 186736.57913967967 140284.15277253836 186724.74131567031</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon>";
+
+            GmlPolygonValidator.IsValid(gml, _gmlReader).Should().BeFalse();
+        }
     }
 }
