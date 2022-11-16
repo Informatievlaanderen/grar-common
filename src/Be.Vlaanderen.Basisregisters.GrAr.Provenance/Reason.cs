510ea8bdd4d68f545812bbd5cef458da43329242
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-11-16 09:14:31 +0100

    fix: no public accessibility for fields

--- src/Be.Vlaanderen.Basisregisters.GrAr.Provenance/Reason.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Provenance/Reason.cs
@@ -4,13 +4,14 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Provenance
 
     public class Reason : StringValueObject<Reason>
     {
-        public static Reason CentralManagementCrab = new Reason("Centrale bijhouding CRAB");
-        public static Reason CentralManagementGrb = new Reason("Centrale bijhouding o.b.v. gebouwinformatie GRB");
-        public static Reason DecentralManagmentCrab = new Reason("Decentrale bijhouding CRAB");
-        public static Reason ManagementCrab = new Reason("Bijhouding op CRAB");
-        public static Reason CentralManagementBPost = new Reason("Centrale bijhouding o.b.v. bPost-bestand");
+        public static readonly Reason CentralManagementCrab = new Reason("Centrale bijhouding CRAB");
+        public static readonly Reason CentralManagementGrb = new Reason("Centrale bijhouding o.b.v. gebouwinformatie GRB");
+        public static readonly Reason DecentralManagmentCrab = new Reason("Decentrale bijhouding CRAB");
+        public static readonly Reason ManagementCrab = new Reason("Bijhouding op CRAB");
+        public static readonly Reason CentralManagementBPost = new Reason("Centrale bijhouding o.b.v. bPost-bestand");
 
-        public Reason(string reason) : base(reason)
+        public Reason(string reason)
+            : base(reason)
         { }
     }
 }
