269ff8fb9335ef956121a43010d8333daf54174b
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-11-16 14:15:39 +0100

    fix: make generic params co/contravariant

--- src/Be.Vlaanderen.Basisregisters.GrAr.Import/Processing/ICommandProcessorBatchConfiguration.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Import/Processing/ICommandProcessorBatchConfiguration.cs
@@ -2,7 +2,7 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Import.Processing
 {
     using System;
 
-    public interface ICommandProcessorBatchConfiguration<TKey>
+    public interface ICommandProcessorBatchConfiguration<out TKey>
     {
         TimeSpan TimeMargin { get; }
         TKey Deserialize(string key);
--- src/Be.Vlaanderen.Basisregisters.GrAr.Import/Processing/IProcessedKeysSet.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Import/Processing/IProcessedKeysSet.cs
@@ -2,7 +2,7 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Import.Processing
 {
     using System.Collections.Generic;
 
-    public interface IProcessedKeysSet<TKey>
+    public interface IProcessedKeysSet<in TKey>
     {
         bool Contains(TKey key);
         void Add(IEnumerable<TKey> keys);
