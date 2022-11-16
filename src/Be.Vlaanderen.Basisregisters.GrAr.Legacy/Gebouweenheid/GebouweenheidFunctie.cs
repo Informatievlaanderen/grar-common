d8a26dd136b74821f2fd1166e129c05c6af9e6b8
jvandaal <jonas.vandaal@outlook.be>
2022-11-16 09:09:30 +0100

    fix: correct gebouweenheidfunctie values according to Oslo values

--- src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/Gebouweenheid/GebouweenheidFunctie.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/Gebouweenheid/GebouweenheidFunctie.cs
@@ -18,6 +18,66 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.Gebouweenheid
         /// Gemeenschappelijk deel.
         /// </summary>
         [EnumMember]
-        GemeenschappelijkDeel = 2
+        GemeenschappelijkDeel = 2,
+
+        /// <summary>
+        /// Wonen.
+        /// </summary>
+        [EnumMember(Value = "Wonen")]
+        Wonen = 3,
+
+        /// <summary>
+        /// Verblijfsrecreatie.
+        /// </summary>
+        [EnumMember(Value = "Verblijfsrecreatie")]
+        Verblijfsrecreatie = 4,
+
+        /// <summary>
+        /// Dagrecreatie, met inbegrip van sport.
+        /// </summary>
+        [EnumMember(Value = "DagrecreatieSport")]
+        DagrecreatieSport = 5,
+
+        /// <summary>
+        /// Land- en tuinbouw in de ruime zin.
+        /// </summary>
+        [EnumMember(Value = "LandTuinbouw")]
+        LandTuinbouw = 6,
+
+        /// <summary>
+        /// Detailhandel.
+        /// </summary>
+        [EnumMember(Value = "Detailhandel")]
+        Detailhandel = 7,
+
+        /// <summary>
+        /// Dancing, restaurant en café.
+        /// </summary>
+        [EnumMember(Value = "DancingRestaurantCafe")]
+        DancingRestaurantCafe = 8,
+
+        /// <summary>
+        /// Kantoorfunctie, dienstverlening en vrije beroepen.
+        /// </summary>
+        [EnumMember(Value = "KantoorDienstverleningVrijBeroep")]
+        KantoorDienstverleningVrijBeroep = 9,
+
+        /// <summary>
+        /// Industrie en bedrijvigheid.
+        /// </summary>
+        [EnumMember(Value = "IndustrieBedrijvigheid")]
+        IndustrieBedrijvigheid = 10,
+
+        /// <summary>
+        /// Gemeenschapsvoorzieningen en openbare nutsvoorzieningen.
+        /// </summary>
+        [EnumMember(Value = "GemeenschapsOpenbareNutsvoorziening")]
+        GemeenschapsOpenbareNutsvoorziening = 11,
+
+        /// <summary>
+        /// Militaire functie.
+        /// </summary>
+        [EnumMember(Value = "MilitairFunctie")]
+        MilitairFunctie = 12
     }
 }
