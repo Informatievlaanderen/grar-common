ced6c9bf3510d7b7d46a4360e9276dbaae734c11
pgallik <patrickgallik@gmail.com>
2022-12-27 12:21:22 +0100

    feat: add capakey to parcel contracts

--- src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/ParcelRegistry/ParcelAddressWasAttachedV2.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/ParcelRegistry/ParcelAddressWasAttachedV2.cs
@@ -7,16 +7,20 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.ParcelRegistry
     {
         public Guid ParcelId { get; }
 
+        public string CaPaKey { get; }
+
         public int AddressPersistentLocalId { get; }
 
         public Provenance Provenance { get; }
 
         public ParcelAddressWasAttachedV2(
             Guid parcelId,
+            string caPaKey,
             int addressPersistentLocalId,
             Provenance provenance)
         {
             ParcelId = parcelId;
+            CaPaKey = caPaKey;
             AddressPersistentLocalId = addressPersistentLocalId;
             Provenance = provenance;
         }
