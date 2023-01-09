72f7f26c70f3ea77227965358d8ab367c8903adf
pgallik <patrickgallik@gmail.com>
2023-01-09 16:40:07 +0100

    feat: add contracts for correct regulate/deregulate address

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/AddressRegistry/AddressRegularizationWasCorrected.cs
@@ -0,0 +1,22 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry
+{
+    using Common;
+
+    public class AddressRegularizationWasCorrected : IQueueMessage
+    {
+        public int StreetNamePersistentLocalId { get; }
+
+        public int AddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public AddressRegularizationWasCorrected(int streetNamePersistentLocalId,
+            int addressPersistentLocalId,
+            Provenance provenance)
+        {
+            StreetNamePersistentLocalId = streetNamePersistentLocalId;
+            AddressPersistentLocalId = addressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
