0c779d9f4e1089cf3dee0f2707c1ea1759166b9e
emalfroy <egon.malfroy@gmail.com>
2023-01-12 13:14:13 +0100

    feat: add BuildingUnitDeregulationWasCorrected

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/BuildingRegistry/BuildingUnitDeregulationWasCorrected.cs
@@ -0,0 +1,22 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.BuildingRegistry
+{
+    using Common;
+
+    public sealed class BuildingUnitDeregulationWasCorrected : IQueueMessage
+    {
+        public int BuildingPersistentLocalId { get; }
+
+        public int BuildingUnitPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public BuildingUnitDeregulationWasCorrected(int buildingPersistentLocalId,
+            int buildingUnitPersistentLocalId,
+            Provenance provenance)
+        {
+            BuildingPersistentLocalId = buildingPersistentLocalId;
+            BuildingUnitPersistentLocalId = buildingUnitPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
