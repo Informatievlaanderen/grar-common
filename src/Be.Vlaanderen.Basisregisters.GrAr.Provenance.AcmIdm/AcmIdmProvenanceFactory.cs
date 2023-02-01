0fd75d1a0f38f339e7c88b693e89d8622a46e587
Arne Dumarey <dumarey.arne@gmail.com>
2023-02-01 16:27:05 +0100

    feat: add provenance.acmidm

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Provenance.AcmIdm/AcmIdmProvenanceFactory.cs
@@ -0,0 +1,52 @@
+﻿namespace Be.Vlaanderen.Basisregisters.GrAr.Provenance.AcmIdm
+{
+    using System;
+    using System.Security.Claims;
+    using Basisregisters.AcmIdm;
+    using Microsoft.AspNetCore.Mvc.Infrastructure;
+    using NodaTime;
+
+    public class AcmIdmProvenanceFactory : IProvenanceFactory
+    {
+        public const string OvoCodeDigitaalVlaanderen = "OVO002949";
+
+        private readonly Application _application;
+        private readonly IActionContextAccessor _actionContextAccessor;
+
+        public AcmIdmProvenanceFactory(
+            Application application,
+            IActionContextAccessor actionContextAccessor)
+        {
+            _application = application;
+            _actionContextAccessor = actionContextAccessor;
+        }
+
+        public Provenance Create(Reason reason, Modification modification)
+        {
+            if (_actionContextAccessor.ActionContext is null)
+            {
+                throw new NullReferenceException("No ActionContext on the ActionContextAccessor");
+            }
+
+            var contextUser = _actionContextAccessor.ActionContext.HttpContext.User;
+            var organisation = Organisation.Other;
+
+            if (contextUser.FindFirstValue(AcmIdmClaimTypes.VoOrgCode) == OvoCodeDigitaalVlaanderen)
+            {
+                organisation = Organisation.DigitaalVlaanderen;
+            }
+            else if (!string.IsNullOrEmpty(contextUser.FindFirstValue(AcmIdmClaimTypes.NisCode)))
+            {
+                organisation = Organisation.Municipality;
+            }
+
+            return new Provenance(
+                SystemClock.Instance.GetCurrentInstant(),
+                _application,
+                reason,
+                new Operator(contextUser.FindFirstValue(AcmIdmClaimTypes.VoOrgCode)),
+                modification,
+                organisation);
+        }
+    }
+}
---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Provenance.AcmIdm/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Basisregisters Vlaanderen")]
+[assembly: AssemblyDescription("GrAr Common Provenance")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("c4ba7cc4-5738-4e13-9cd0-a0d29af269e7")]
