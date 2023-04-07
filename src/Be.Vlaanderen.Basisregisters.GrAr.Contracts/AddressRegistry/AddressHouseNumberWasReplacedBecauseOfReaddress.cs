0009c8202109ee90ab1eed26ec762a6d1a91c504
jvandaal <jonas.vandaal@outlook.be>
2023-04-07 08:20:08 +0200

    feat: add AddressHouseNumberWasReplacedBecauseOfReaddress

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/AddressRegistry/AddressHouseNumberWasReplacedBecauseOfReaddress.cs
@@ -0,0 +1,34 @@
+﻿namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry
+{
+    using System.Collections.Generic;
+    using Common;
+
+    public class AddressHouseNumberWasReplacedBecauseOfReaddress : IQueueMessage
+    {
+        public int StreetNamePersistentLocalId { get; }
+
+        public int DestinationStreetNamePersistentLocalId { get; }
+
+        public int AddressPersistentLocalId { get; }
+
+        public int DestinationAddressPersistentLocalId { get; }
+
+        public IList<AddressBoxNumberReplacedBecauseOfReaddressData> BoxNumberAddressPersistentLocalIds { get;  }
+
+        public Provenance Provenance { get; private set; }
+    }
+
+    public class AddressBoxNumberReplacedBecauseOfReaddressData
+    {
+        public int SourceAddressPersistentLocalId { get; }
+        public int DestinationAddressPersistentLocalId { get; }
+
+        public AddressBoxNumberReplacedBecauseOfReaddressData(
+            int sourceAddressPersistentLocalId,
+            int destinationAddressPersistentLocalId)
+        {
+            SourceAddressPersistentLocalId = sourceAddressPersistentLocalId;
+            DestinationAddressPersistentLocalId = destinationAddressPersistentLocalId;
+        }
+    }
+}
