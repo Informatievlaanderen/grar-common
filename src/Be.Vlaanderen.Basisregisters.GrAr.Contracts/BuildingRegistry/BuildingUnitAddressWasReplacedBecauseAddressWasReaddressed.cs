5727884d4fb972dc39e340ae724ef483869eebbf
pgallik <patrickgallik@gmail.com>
2023-04-07 14:51:17 +0200

    feat: add BuildingUnitAddressAttachmentWasReplacedBecauseAddressWasReaddressed

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/BuildingRegistry/BuildingUnitAddressWasReplacedBecauseAddressWasReaddressed.cs
@@ -0,0 +1,31 @@
+﻿namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.BuildingRegistry
+{
+    using Common;
+
+    public class BuildingUnitAddressWasReplacedBecauseAddressWasReaddressed : IQueueMessage
+    {
+        public int BuildingPersistentLocalId { get; }
+
+        public int BuildingUnitPersistentLocalId { get; }
+
+        public int SourceAddressPersistentLocalId { get; }
+
+        public int DestinationAddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public BuildingUnitAddressWasReplacedBecauseAddressWasReaddressed(
+            int buildingPersistentLocalId,
+            int buildingUnitPersistentLocalId,
+            int sourceAddressPersistentLocalId,
+            int destinationAddressPersistentLocalId,
+            Provenance provenance)
+        {
+            BuildingPersistentLocalId = buildingPersistentLocalId;
+            BuildingUnitPersistentLocalId = buildingUnitPersistentLocalId;
+            SourceAddressPersistentLocalId = sourceAddressPersistentLocalId;
+            DestinationAddressPersistentLocalId = destinationAddressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
