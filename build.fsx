92a0f16ac75a091f494415c2a55b0a71f59bbc9d
jvandaal <jonas.vandaal@outlook.be>
2023-04-12 10:55:07 +0200

    fix: bump to trigger build

--- build.fsx
+++ build.fsx
@@ -13,7 +13,6 @@ open ``Build-generic``
 
 let assemblyVersionNumber = (sprintf "%s.0")
 let nugetVersionNumber = (sprintf "%s")
-
 let buildSolution = buildSolution assemblyVersionNumber
 let buildSource = build assemblyVersionNumber
 let buildTest = buildTest assemblyVersionNumber
