113c76a43ed167ccecac58f3de1948b4c3da3c79
jvandaal <jonas.vandaal@outlook.be>
2023-04-28 12:08:14 +0200

    fix: extend BuildingWasMeasured with building units

--- src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/BuildingRegistry/BuildingWasMeasured.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/BuildingRegistry/BuildingWasMeasured.cs
@@ -1,22 +1,35 @@
 namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.BuildingRegistry
 {
+    using System.Collections.Generic;
     using Common;
 
     public sealed class BuildingWasMeasured : IQueueMessage
     {
         public int BuildingPersistentLocalId { get; }
 
-        public string ExtendedWkbGeometry { get; }
+        public List<int> BuildingUnitPersistentLocalIds { get; }
+
+        public List<int> BuildingUnitPersistentLocalIdsWhichBecameDerived { get; }
+
+        public string ExtendedWkbGeometryBuilding { get; }
+
+        public string? ExtendedWkbGeometryBuildingUnits { get; }
 
         public Provenance Provenance { get; }
 
         public BuildingWasMeasured(
             int buildingPersistentLocalId,
-            string extendedWkbGeometry,
+            List<int> buildingUnitPersistentLocalIds,
+            List<int> buildingUnitPersistentLocalIdsWhichBecameDerived,
+            string extendedWkbGeometryBuilding,
+            string? extendedWkbGeometryBuildingUnits,
             Provenance provenance)
         {
             BuildingPersistentLocalId = buildingPersistentLocalId;
-            ExtendedWkbGeometry = extendedWkbGeometry;
+            BuildingUnitPersistentLocalIds = buildingUnitPersistentLocalIds;
+            BuildingUnitPersistentLocalIdsWhichBecameDerived = buildingUnitPersistentLocalIdsWhichBecameDerived;
+            ExtendedWkbGeometryBuilding = extendedWkbGeometryBuilding;
+            ExtendedWkbGeometryBuildingUnits = extendedWkbGeometryBuildingUnits;
             Provenance = provenance;
         }
     }
