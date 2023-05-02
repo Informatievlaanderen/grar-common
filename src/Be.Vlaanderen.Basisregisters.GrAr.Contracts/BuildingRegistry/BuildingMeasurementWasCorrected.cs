07faa7dc8ab7e23a9f82dc077d74f92d5410a0b1
jvandaal <jonas.vandaal@outlook.be>
2023-05-02 09:29:35 +0200

    feat: add BuildingMeasurementWasCorrected

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/BuildingRegistry/BuildingMeasurementWasCorrected.cs
@@ -0,0 +1,36 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.BuildingRegistry
+{
+    using System.Collections.Generic;
+    using Common;
+
+    public sealed class BuildingMeasurementWasCorrected : IQueueMessage
+    {
+        public int BuildingPersistentLocalId { get; }
+
+        public List<int> BuildingUnitPersistentLocalIds { get; }
+
+        public List<int> BuildingUnitPersistentLocalIdsWhichBecameDerived { get; }
+
+        public string ExtendedWkbGeometryBuilding { get; }
+
+        public string? ExtendedWkbGeometryBuildingUnits { get; }
+
+        public Provenance Provenance { get; }
+
+        public BuildingMeasurementWasCorrected(
+            int buildingPersistentLocalId,
+            List<int> buildingUnitPersistentLocalIds,
+            List<int> buildingUnitPersistentLocalIdsWhichBecameDerived,
+            string extendedWkbGeometryBuilding,
+            string? extendedWkbGeometryBuildingUnits,
+            Provenance provenance)
+        {
+            BuildingPersistentLocalId = buildingPersistentLocalId;
+            BuildingUnitPersistentLocalIds = buildingUnitPersistentLocalIds;
+            BuildingUnitPersistentLocalIdsWhichBecameDerived = buildingUnitPersistentLocalIdsWhichBecameDerived;
+            ExtendedWkbGeometryBuilding = extendedWkbGeometryBuilding;
+            ExtendedWkbGeometryBuildingUnits = extendedWkbGeometryBuildingUnits;
+            Provenance = provenance;
+        }
+    }
+}
