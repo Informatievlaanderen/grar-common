0279950938f0d412891070d133f2a08d63595e06
Rik De Peuter <rikdepeuter@gmail.com>
2023-06-01 09:50:21 +0200

    fix: WR-725 add GeoJSONMultiLineString / GeoJSONLineString

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/SpatialTools/GeometryCoordinates/LineStringCoordinatesConverter.cs
@@ -0,0 +1,37 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.SpatialTools
+{
+    using System;
+    using System.Linq;
+    using Newtonsoft.Json;
+
+    public class LineStringCoordinatesConverter : JsonConverter<double[][]>
+    {
+        private static readonly object Lock = new object();
+
+        public override void WriteJson(
+            JsonWriter writer,
+            double[][] lineString,
+            JsonSerializer serializer)
+        {
+            lock (Lock)
+            {
+                if (!serializer.Converters.Any(converter => converter is GeometryCoordinateValueConverter))
+                    serializer.Converters.Add(new GeometryCoordinateValueConverter());
+            }
+
+            serializer.Serialize(
+                writer,
+                lineString.Select(points =>
+                    points.Select(coordinateValue => new LineStringGeometryCoordinateValue(coordinateValue))),
+                typeof(GeometryCoordinateValueConverter));
+        }
+
+        public override double[][] ReadJson(
+            JsonReader reader,
+            Type objectType,
+            double[][] existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer)
+            => throw new NotImplementedException($"Json deserialization of LineStringCoordinates is not supported");
+    }
+}
---
+++ test/Be.Vlaanderen.Basisregisters.GrAr.Tests/GeometryCoordinates/LineStringGeometryCoordinateValueTests.cs
@@ -0,0 +1,118 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Tests.GeometryCoordinates
+{
+    using System;
+    using System.Globalization;
+    using AutoFixture;
+    using FluentAssertions;
+    using Infrastructure;
+    using Legacy.SpatialTools;
+    using Xunit;
+
+    public class WhenConvertingACreatingALineStringGeometryCoordinateValueBackToADouble
+    {
+        private readonly double _originalValue;
+        private readonly GeometryCoordinateValue _coordinateValue;
+
+        public WhenConvertingACreatingALineStringGeometryCoordinateValueBackToADouble()
+        {
+            _originalValue = new Fixture().CreateDoubleWithPrecision(12, 25);
+
+            _coordinateValue = new LineStringGeometryCoordinateValue(_originalValue);
+        }
+
+        [Fact]
+        public void ThenOriginalPrecisionShouldBeMaintained()
+        {
+            _coordinateValue
+                .Should()
+                .Be(_originalValue);
+        }
+
+    }
+
+    public class WhenWritingALineStringGeometryCoordinateValueWithLessPrecisionThanExpected
+    {
+        private readonly double _originalValue;
+        private readonly string _printedValue;
+
+        public WhenWritingALineStringGeometryCoordinateValueWithLessPrecisionThanExpected()
+        {
+            _originalValue = new Fixture().CreateDoubleWithPrecision(0, 10);
+
+            _printedValue = new LineStringGeometryCoordinateValue(_originalValue).ToString();
+        }
+
+        [Fact]
+        public void ThenTheDecimalPrecisionShouldBeExtendedToShowElevenDecimals()
+        {
+            _printedValue
+                .Should()
+                .MatchRegex(@"^-?\d+\.\d{11}$");
+        }
+
+        [Fact]
+        public void ThenPrintedValueShouldBeEqualToTheOriginal()
+        {
+            double.Parse(_printedValue, CultureInfo.InvariantCulture)
+                .Should()
+                .Be(_originalValue);
+        }
+    }
+
+    public class WhenWritingALineStringGeometryCoordinateValueWithMorePrecisionThanSupported
+    {
+        private readonly double _originalValue;
+        private readonly string _printedValue;
+
+        public WhenWritingALineStringGeometryCoordinateValueWithMorePrecisionThanSupported()
+        {
+            _originalValue = new Fixture().CreateDoubleWithPrecision(12, 20);
+
+            _printedValue = new LineStringGeometryCoordinateValue(_originalValue).ToString();
+        }
+
+        [Fact]
+        public void ThenTheDecimalPrecisionShouldBeReducedToElevenDecimals()
+        {
+            _printedValue
+                .Should()
+                .MatchRegex(@"^-?\d+\.\d{11}$");
+        }
+
+        [Fact]
+        public void ThenThePrintedValueShouldBeRoundedToElevenDecimalsPrecision()
+        {
+            var roundedValue = Math.Round(_originalValue, 11);
+            double.Parse(_printedValue, CultureInfo.InvariantCulture)
+                .Should()
+                .Be(roundedValue);
+        }
+    }
+
+    public class WhenParsingAValidDoubleStringAsLineStringGeometryCoordinateValue
+    {
+        [Fact]
+        public void ThenAGeometryCoordinateValueShouldBeReturned()
+        {
+            var value = new Fixture().CreateDoubleWithPrecision(15);
+            LineStringGeometryCoordinateValue
+                .TryParse(value.ToString(CultureInfo.InvariantCulture))
+                .Should()
+                .NotBeNull()
+                .And.BeOfType<LineStringGeometryCoordinateValue>()
+                .And.Be(value);
+        }
+    }
+
+    public class WhenParsingAnInvalidDoubleStringAsLineStringGeometryCoordinateValue
+    {
+        [Fact]
+        public void ThenNothingShouldBeReturned()
+        {
+            LineStringGeometryCoordinateValue
+                .TryParse(new Fixture().Create<string>())
+                .Should()
+                .BeNull();
+        }
+    }
+}
