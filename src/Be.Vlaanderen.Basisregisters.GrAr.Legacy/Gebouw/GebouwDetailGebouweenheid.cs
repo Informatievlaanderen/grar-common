5b75327caeeeeed8b7f627df4d976e388772677c
jvandaal <jonas.vandaal@outlook.be>
2023-10-04 16:12:16 +0200

    fix: GebouwDetailGebouweenheid Status should be enum

--- src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/Gebouw/GebouwDetailGebouweenheid.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Legacy/Gebouw/GebouwDetailGebouweenheid.cs
@@ -1,6 +1,7 @@
 namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.Gebouw
 {
     using System.Runtime.Serialization;
+    using Gebouweenheid;
     using Newtonsoft.Json;
 
     [DataContract(Name = "Gebouweenheid", Namespace = "")]
@@ -18,7 +19,7 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.Gebouw
         /// </summary>
         [DataMember(Name = "Status", Order = 2)]
         [JsonProperty(Required = Required.DisallowNull)]
-        public string Status { get; set; }
+        public GebouweenheidStatus Status { get; set; }
 
         /// <summary>
         /// De URL die de details van de meest recente versie van de gekoppelde gebouweenheid weergeeft.
@@ -27,7 +28,7 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Legacy.Gebouw
         [JsonProperty(Required = Required.DisallowNull)]
         public string Detail { get; set; }
 
-        public GebouwDetailGebouweenheid(string objectId, string status, string detail)
+        public GebouwDetailGebouweenheid(string objectId, GebouweenheidStatus status, string detail)
         {
             ObjectId = objectId;
             Status = status;
