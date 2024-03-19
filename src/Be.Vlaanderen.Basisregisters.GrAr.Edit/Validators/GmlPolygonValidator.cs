2b2fc8a5ac72ee6a3ace60fd69f886ef153b75f3
Arne Dumarey <dumarey.arne@gmail.com>
2024-03-19 21:33:52 +0100

    feat: add validopfactory for GML reader

--- src/Be.Vlaanderen.Basisregisters.GrAr.Edit/Validators/GmlPolygonValidator.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Edit/Validators/GmlPolygonValidator.cs
@@ -4,10 +4,19 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Edit.Validators
     using System.Xml;
     using NetTopologySuite.Geometries;
     using NetTopologySuite.IO.GML2;
+    using NetTopologySuite.Operation.Valid;
 
     public static class GmlPolygonValidator
     {
         public static bool IsValid(string? gml, GMLReader gmlReader)
+        {
+            return IsValid(gml, gmlReader, null);
+        }
+
+        public static bool IsValid(
+            string? gml,
+            GMLReader gmlReader,
+            Func<Geometry, IsValidOp>? isValidOpFactory)
         {
             if (string.IsNullOrEmpty(gml) || !gml.Contains(GmlConstants.GmlVersionAttribute) || !gml.Contains(GmlConstants.SrsNameAttribute))
             {
@@ -18,7 +27,7 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Edit.Validators
             {
                 var geometry = gmlReader.Read(gml);
 
-                return geometry is Polygon && geometry.IsValid;
+                return geometry is Polygon && (isValidOpFactory is null ? geometry.IsValid : isValidOpFactory(geometry).IsValid);
             }
             catch (XmlException)
             {
--- test/Be.Vlaanderen.Basisregisters.GrAr.Tests/Edit/ValidatorTests/GmlPolygonValidatorTests.cs
+++ test/Be.Vlaanderen.Basisregisters.GrAr.Tests/Edit/ValidatorTests/GmlPolygonValidatorTests.cs
@@ -23,9 +23,27 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Tests.Edit.ValidatorTests
         [InlineData("<gml:Polygon xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:exterior><gml:LinearRing><gml:posList>140284.15277253836 186724.74131567031 140291.06016454101 186726.38355567306 140288.22675654292 186738.25798767805 140281.19098053873 186736.57913967967 140284.15277253836 186724.74131567031</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon>", false)]
         [InlineData("<gml:Polygon srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\"><gml:exterior><gml:LinearRing><gml:posList>140284.15277253836 186724.74131567031 140291.06016454101 186726.38355567306 140288.22675654292 186738.25798767805 140281.19098053873 186736.57913967967 140284.15277253836 186724.74131567031</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon>", false)]
         [InlineData("<gml:Polygon srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:exterior><gml:LinearRing><gml:posList>140284.15277253836 186724.74131567031 140291.06016454101 186726.38355567306 140288.22675654292 186738.25798767805 140281.19098053873 186736.57913967967 140284.15277253836 186724.74131567031</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon>", true)]
+        // This is a selftouchingring, same as test below. By default INVALID!
+        [InlineData("<gml:Polygon srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:exterior><gml:LinearRing><gml:posList>30359.924344554543 197007.54170677811 30359.446008555591 197010.21338678151 30371.943992562592 197013.23297078162 30373.701176568866 197006.42113077641 30363.939512558281 197004.00340277702 30364.205112561584 197002.85997877643 30357.719608552754 197001.36161077395 30356.638264551759 197006.90023477748 30359.924344554543 197007.54170677811 30360.468344554305 197004.48564277589 30362.562808558345 197004.85844277591 30362.018680557609 197007.91457077861 30359.924344554543 197007.54170677811</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon>", false)]
         public void GivenGml_ThenExpectedResult(string gml, bool expectedResult)
         {
             GmlPolygonValidator.IsValid(gml, _gmlReader).Should().Be(expectedResult);
+        }
+
+        [Fact]
+        public void GivenGmlSelfTouchingIntersectionValidOp_ThenGmlIsValid()
+        {
+            var validOp = (Geometry geometry) =>
+                new NetTopologySuite.Operation.Valid.IsValidOp(geometry)
+                {
+                    IsSelfTouchingRingFormingHoleValid = true,
+                    SelfTouchingRingFormingHoleValid = true
+                };
+
+            var gml =
+                "<gml:Polygon srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:exterior><gml:LinearRing><gml:posList>30359.924344554543 197007.54170677811 30359.446008555591 197010.21338678151 30371.943992562592 197013.23297078162 30373.701176568866 197006.42113077641 30363.939512558281 197004.00340277702 30364.205112561584 197002.85997877643 30357.719608552754 197001.36161077395 30356.638264551759 197006.90023477748 30359.924344554543 197007.54170677811 30360.468344554305 197004.48564277589 30362.562808558345 197004.85844277591 30362.018680557609 197007.91457077861 30359.924344554543 197007.54170677811</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon>";
+
+            GmlPolygonValidator.IsValid(gml, _gmlReader, validOp).Should().BeTrue();
         }
 
         [Fact]
