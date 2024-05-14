66bfd9d3316811e988ca880053211faf6516d7a0
Arne Dumarey <dumarey.arne@gmail.com>
2024-05-14 14:12:32 +0200

    feat(contracts): add streetname was readdressed

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/AddressRegistry/StreetNameWasReaddressed.cs
@@ -0,0 +1,43 @@
+﻿namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry
+{
+    using System.Collections.Generic;
+    using Common;
+
+    public class StreetNameWasReaddressed : IQueueMessage
+    {
+        public int StreetNamePersistentLocalId { get; }
+
+        public IReadOnlyList<AddressHouseNumberReaddressedData> ReaddressedHouseNumbers { get; }
+
+        public Provenance Provenance { get; }
+
+        public StreetNameWasReaddressed(
+            int streetNamePersistentLocalId,
+            IReadOnlyList<AddressHouseNumberReaddressedData> readdressedHouseNumbers,
+            Provenance provenance)
+        {
+            StreetNamePersistentLocalId = streetNamePersistentLocalId;
+            ReaddressedHouseNumbers = readdressedHouseNumbers;
+            Provenance = provenance;
+        }
+    }
+
+    public class AddressHouseNumberReaddressedData
+    {
+        public int AddressPersistentLocalId { get; }
+
+        public ReaddressedAddressData ReaddressedHouseNumber { get; }
+
+        public IReadOnlyList<ReaddressedAddressData> ReaddressedBoxNumbers { get; }
+
+        public AddressHouseNumberReaddressedData(
+            int addressPersistentLocalId,
+            ReaddressedAddressData readdressedHouseNumber,
+            IReadOnlyList<ReaddressedAddressData> readdressedBoxNumbers)
+        {
+            AddressPersistentLocalId = addressPersistentLocalId;
+            ReaddressedHouseNumber = readdressedHouseNumber;
+            ReaddressedBoxNumbers = readdressedBoxNumbers;
+        }
+    }
+}
