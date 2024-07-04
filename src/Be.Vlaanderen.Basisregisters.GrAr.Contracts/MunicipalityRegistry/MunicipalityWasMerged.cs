ab35720a6ff646d06b93265acb044f04011433be
Arne Dumarey <dumarey.arne@gmail.com>
2024-07-04 16:17:34 +0200

    feat(contracts): add municipalitywasmerged

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/MunicipalityRegistry/MunicipalityWasMerged.cs
@@ -0,0 +1,40 @@
+﻿namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.MunicipalityRegistry
+{
+    using System.Collections.Generic;
+    using Common;
+
+    public sealed class MunicipalityWasMerged : IQueueMessage
+    {
+        public string MunicipalityId { get; }
+
+        public string NisCode { get; }
+
+        public IEnumerable<string> MunicipalityIdsToMergeWith { get; }
+
+        public IEnumerable<string> NisCodesToMergeWith { get; }
+
+        public string NewMunicipalityId { get; }
+
+        public string NewNisCode { get; }
+
+        public Provenance Provenance { get; private set; }
+
+        public MunicipalityWasMerged(
+            string municipalityId,
+            string nisCode,
+            IEnumerable<string> municipalityIdsToMergeWith,
+            IEnumerable<string> nisCodesToMergeWith,
+            string newMunicipalityId,
+            string newNisCode,
+            Provenance provenance)
+        {
+            MunicipalityId = municipalityId;
+            NisCode = nisCode;
+            MunicipalityIdsToMergeWith = municipalityIdsToMergeWith;
+            NisCodesToMergeWith = nisCodesToMergeWith;
+            NewMunicipalityId = newMunicipalityId;
+            NewNisCode = newNisCode;
+            Provenance = provenance;
+        }
+    }
+}
