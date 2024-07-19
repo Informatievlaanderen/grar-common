a99594dca75588d8411832bdc0c830543e54eed5
jvandaal <jonas.vandaal@outlook.be>
2024-07-19 15:24:08 +0200

    feat: add AddressRegistry municipality merger events

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/AddressRegistry/AddressWasRejectedBecauseOfMunicipalityMerger.cs
@@ -0,0 +1,27 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry
+{
+    using Common;
+
+    public class AddressWasRejectedBecauseOfMunicipalityMerger : IQueueMessage
+    {
+        public int StreetNamePersistentLocalId { get; }
+
+        public int AddressPersistentLocalId { get; }
+
+        public int NewAddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public AddressWasRejectedBecauseOfMunicipalityMerger(
+            int streetNamePersistentLocalId,
+            int addressPersistentLocalId,
+            int newAddressPersistentLocalId,
+            Provenance provenance)
+        {
+            StreetNamePersistentLocalId = streetNamePersistentLocalId;
+            AddressPersistentLocalId = addressPersistentLocalId;
+            NewAddressPersistentLocalId = newAddressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/AddressRegistry/AddressWasRetiredBecauseOfMunicipalityMerger.cs
@@ -0,0 +1,27 @@
+namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry
+{
+    using Common;
+
+    public class AddressWasRetiredBecauseOfMunicipalityMerger : IQueueMessage
+    {
+        public int StreetNamePersistentLocalId { get; }
+
+        public int AddressPersistentLocalId { get; }
+
+        public int NewAddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public AddressWasRetiredBecauseOfMunicipalityMerger(
+            int streetNamePersistentLocalId,
+            int addressPersistentLocalId,
+            int newAddressPersistentLocalId,
+            Provenance provenance)
+        {
+            StreetNamePersistentLocalId = streetNamePersistentLocalId;
+            AddressPersistentLocalId = addressPersistentLocalId;
+            NewAddressPersistentLocalId = newAddressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
