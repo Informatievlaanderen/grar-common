af2a9f125b32c5fdd4f004dde062a24d55d20c15
jvandaal <jonas.vandaal@outlook.be>
2024-07-23 15:14:32 +0200

    feat: add ParcelAddressWasReplacedBecauseOfMunicipalityMerger

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/ParcelRegistry/ParcelAddressWasReplacedBecauseOfMunicipalityMerger.cs
@@ -0,0 +1,31 @@
+﻿namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.ParcelRegistry
+{
+    using Common;
+
+    public class ParcelAddressWasReplacedBecauseOfMunicipalityMerger : IQueueMessage
+    {
+        public string ParcelId { get; }
+
+        public string CaPaKey { get; }
+
+        public int NewAddressPersistentLocalId { get; }
+
+        public int PreviousAddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public ParcelAddressWasReplacedBecauseOfMunicipalityMerger(
+            string parcelId,
+            string caPaKey,
+            int newAddressPersistentLocalId,
+            int previousAddressPersistentLocalId,
+            Provenance provenance)
+        {
+            ParcelId = parcelId;
+            CaPaKey = caPaKey;
+            NewAddressPersistentLocalId = newAddressPersistentLocalId;
+            PreviousAddressPersistentLocalId = previousAddressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
