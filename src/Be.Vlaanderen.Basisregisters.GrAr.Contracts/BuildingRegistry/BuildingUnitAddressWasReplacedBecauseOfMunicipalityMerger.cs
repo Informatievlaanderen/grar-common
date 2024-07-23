9839da30d4af5c1f5f8a8a6a23eabe2aa28b7cb9
jvandaal <jonas.vandaal@outlook.be>
2024-07-23 10:16:19 +0200

    feat: BuildingUnitAddressWasReplacedBecauseOfMunicipalityMerger

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/BuildingRegistry/BuildingUnitAddressWasReplacedBecauseOfMunicipalityMerger.cs
@@ -0,0 +1,31 @@
+﻿namespace Be.Vlaanderen.Basisregisters.GrAr.Contracts.BuildingRegistry
+{
+    using Common;
+
+    public class BuildingUnitAddressWasReplacedBecauseOfMunicipalityMerger : IQueueMessage
+    {
+        public int BuildingPersistentLocalId { get; }
+
+        public int BuildingUnitPersistentLocalId { get; }
+
+        public int NewAddressPersistentLocalId { get; }
+
+        public int PreviousAddressPersistentLocalId { get; }
+
+        public Provenance Provenance { get; }
+
+        public BuildingUnitAddressWasReplacedBecauseOfMunicipalityMerger(
+            int buildingPersistentLocalId,
+            int buildingUnitPersistentLocalId,
+            int newAddressPersistentLocalId,
+            int previousAddressPersistentLocalId,
+            Provenance provenance)
+        {
+            BuildingPersistentLocalId = buildingPersistentLocalId;
+            BuildingUnitPersistentLocalId = buildingUnitPersistentLocalId;
+            NewAddressPersistentLocalId = newAddressPersistentLocalId;
+            PreviousAddressPersistentLocalId = previousAddressPersistentLocalId;
+            Provenance = provenance;
+        }
+    }
+}
