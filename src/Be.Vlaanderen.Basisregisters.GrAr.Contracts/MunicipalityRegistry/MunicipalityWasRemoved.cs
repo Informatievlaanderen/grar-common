3649c93ed42a75117026e8e30944e7444a85c169
Arne Dumarey <dumarey.arne@gmail.com>
2025-07-15 08:47:25 +0200

    fix(contract): add NisCode to MunicipalityWasRemoved

--- src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/MunicipalityRegistry/MunicipalityWasRemoved.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Contracts/MunicipalityRegistry/MunicipalityWasRemoved.cs
@@ -5,14 +5,16 @@
     public class MunicipalityWasRemoved : IQueueMessage
     {
         public string MunicipalityId { get; }
-
+        public string NisCode { get; }
         public Provenance Provenance { get; }
 
         public MunicipalityWasRemoved(
             string municipalityId,
+            string nisCode,
             Provenance provenance)
         {
             MunicipalityId = municipalityId;
+            NisCode = nisCode;
             Provenance = provenance;
         }
     }
