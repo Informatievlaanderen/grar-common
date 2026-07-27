c7e5e535b51aadcd46ed80394cf09d9cda532368
Rik De Peuter <rikdepeuter@gmail.com>
2026-07-27 14:24:53 +0200

    feat(common): add optional coordinate precision to ConvertToGml
    ConvertToGml gains an optional coordinatePrecision (int?) parameter. When null
    (default) the existing geometry-type based precision is used (2 decimals for a
    point, 11 for polygon/multipolygon/linestring); when specified, that number of
    decimals is used for every coordinate instead.

--- test/Be.Vlaanderen.Basisregisters.GrAr.Tests/Common/NTS/GeometryExtensionsTests.cs
+++ test/Be.Vlaanderen.Basisregisters.GrAr.Tests/Common/NTS/GeometryExtensionsTests.cs
@@ -1,5 +1,6 @@
 namespace Be.Vlaanderen.Basisregisters.GrAr.Tests.Common.NTS
 {
+    using System;
     using FluentAssertions;
     using GrAr.Common.NetTopology;
     using NetTopologySuite.Geometries;
@@ -8,6 +9,101 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Tests.Common.NTS
 
     public class GeometryExtensionsTests
     {
+        [Fact]
+        public void WhenConvertLineStringToGmlWithCoordinatePrecision_ThenCoordinatesUseThatPrecision()
+        {
+            var geometry = new LineString(
+            [
+                new Coordinate(10, 20),
+                new Coordinate(30, 40)
+            ]);
+            geometry.SRID = SystemReferenceId.SridLambert72;
+
+            var resultingGml = geometry.ConvertToGml(true, 2);
+
+            resultingGml.Should().Be(
+                "<gml:LineString srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\">" +
+                "<gml:posList>10.00 20.00 30.00 40.00</gml:posList>" +
+                "</gml:LineString>");
+        }
+
+        [Fact]
+        public void WhenConvertLineStringToGmlWithCoordinatePrecisionZero_ThenCoordinatesHaveNoDecimals()
+        {
+            var geometry = new LineString(
+            [
+                new Coordinate(10, 20),
+                new Coordinate(30, 40)
+            ]);
+            geometry.SRID = SystemReferenceId.SridLambert72;
+
+            var resultingGml = geometry.ConvertToGml(true, 0);
+
+            resultingGml.Should().Be(
+                "<gml:LineString srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\">" +
+                "<gml:posList>10 20 30 40</gml:posList>" +
+                "</gml:LineString>");
+        }
+
+        [Fact]
+        public void WhenConvertPointToGmlWithCoordinatePrecision_ThenCoordinatesUseThatPrecision()
+        {
+            var geometry = new Point(new Coordinate(1.123456, 2.5)) { SRID = SystemReferenceId.SridLambert72 };
+
+            var resultingGml = geometry.ConvertToGml(true, 5);
+
+            resultingGml.Should().Be(
+                "<gml:Point srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\">" +
+                "<gml:pos>1.12346 2.50000</gml:pos>" +
+                "</gml:Point>");
+        }
+
+        [Fact]
+        public void WhenConvertPolygonToGmlWithCoordinatePrecision_ThenCoordinatesUseThatPrecision()
+        {
+            var geometry = new Polygon(new LinearRing(
+            [
+                new Coordinate(0, 0),
+                new Coordinate(0, 10),
+                new Coordinate(10, 10),
+                new Coordinate(10, 0),
+                new Coordinate(0, 0)
+            ]));
+            geometry.SRID = SystemReferenceId.SridLambert72;
+
+            var resultingGml = geometry.ConvertToGml(true, 1);
+
+            resultingGml.Should().Be(
+                "<gml:Polygon srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\">" +
+                "<gml:exterior><gml:LinearRing>" +
+                "<gml:posList>0.0 0.0 0.0 10.0 10.0 10.0 10.0 0.0 0.0 0.0</gml:posList>" +
+                "</gml:LinearRing></gml:exterior>" +
+                "</gml:Polygon>");
+        }
+
+        [Fact]
+        public void WhenConvertToGmlWithNullCoordinatePrecision_ThenUsesGeometryTypeBasedPrecision()
+        {
+            var geometry = new LineString(
+            [
+                new Coordinate(10, 20),
+                new Coordinate(30, 40)
+            ]);
+            geometry.SRID = SystemReferenceId.SridLambert72;
+
+            geometry.ConvertToGml(true, null).Should().Be(geometry.ConvertToGml(true));
+        }
+
+        [Fact]
+        public void WhenConvertToGmlWithNegativeCoordinatePrecision_ThenThrows()
+        {
+            var geometry = new Point(new Coordinate(1, 2)) { SRID = SystemReferenceId.SridLambert72 };
+
+            var act = () => geometry.ConvertToGml(true, -1);
+
+            act.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
         [Fact]
         public void WhenConvertPolygonToGml_ThenProduceValidXml()
         {
