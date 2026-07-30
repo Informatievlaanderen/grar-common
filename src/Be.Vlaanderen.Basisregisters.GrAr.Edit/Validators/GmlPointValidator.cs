0619dcb18486c97bc3f2aa6f9263f61f7d0ca0cf
Arne Dumarey <dumarey.arne@gmail.com>
2026-07-30 17:30:30 +0200

    feat(edit): add point validator to handle lambert2008

--- src/Be.Vlaanderen.Basisregisters.GrAr.Edit/Validators/GmlPointValidator.cs
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Edit/Validators/GmlPointValidator.cs
@@ -1,11 +1,14 @@
 namespace Be.Vlaanderen.Basisregisters.GrAr.Edit.Validators
 {
     using System.Xml;
+    using Common;
+    using Common.NetTopology;
     using NetTopologySuite.Geometries;
     using NetTopologySuite.IO.GML2;
 
     public static class GmlPointValidator
     {
+        private const string GmlVersionAttributeValue = "http://www.opengis.net/gml/3.2";
         public static bool IsValid(string? gml, GMLReader gmlReader)
         {
             return IsValid(gml, gmlReader, out _);
@@ -14,7 +17,7 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Edit.Validators
         public static bool IsValid(string? gml, GMLReader gmlReader, out Point? point)
         {
             point = null;
-            if (string.IsNullOrEmpty(gml) || !gml.Contains(GmlConstants.GmlVersionAttribute) || !gml.Contains(GmlConstants.SrsNameAttribute))
+            if (string.IsNullOrEmpty(gml) || !gml.Contains(GmlVersionAttributeValue) || !gml.Contains(GmlConstants.SrsNameAttribute))
             {
                 return false;
             }
@@ -36,5 +39,43 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Edit.Validators
                 return false;
             }
         }
+
+        /// <summary>
+        /// Validates a GML point position. Unlike IsValid(string, GMLReader, out Point?)
+        /// this accepts both Lambert 72 (EPSG 31370) and Lambert 2008 (EPSG 3812) as srsName.
+        /// </summary>
+        public static bool IsValidPoint(string? gml, out Point? point)
+        {
+            point = null;
+            // srsName check is moved to TryReadSridGml
+            if (string.IsNullOrEmpty(gml) || !gml.Contains(GmlVersionAttributeValue))
+            {
+                return false;
+            }
+
+            if (!gml.TryReadSridGml(out var srid))
+            {
+                return false;
+            }
+
+            try
+            {
+                var geometry = GmlFactory.CreateGmlReader(srid).Read(gml);
+
+                if (geometry is Point { IsValid: true } gmlPoint)
+                {
+                    point = gmlPoint;
+                    return true;
+                }
+
+                return false;
+            }
+            catch (XmlException)
+            {
+                return false;
+            }
+        }
+
+        public static bool IsValidPoint(string? gml) => IsValidPoint(gml, out _);
     }
 }
--- test/Be.Vlaanderen.Basisregisters.GrAr.Tests/Edit/ValidatorTests/GmlPointValidatorTests.cs
+++ test/Be.Vlaanderen.Basisregisters.GrAr.Tests/Edit/ValidatorTests/GmlPointValidatorTests.cs
@@ -1,6 +1,8 @@
 namespace Be.Vlaanderen.Basisregisters.GrAr.Tests.Edit.ValidatorTests
 {
+    using System;
     using FluentAssertions;
+    using GrAr.Common.NetTopology;
     using GrAr.Edit.Validators;
     using NetTopologySuite.Geometries;
     using NetTopologySuite.IO.GML2;
@@ -8,12 +10,17 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Tests.Edit.ValidatorTests
 
     public class GmlPointValidatorTests
     {
+        private const string GmlNamespace = "xmlns:gml=\"http://www.opengis.net/gml/3.2\"";
+
         private readonly GMLReader _gmlReader;
 
         public GmlPointValidatorTests()
         {
             _gmlReader = new GMLReader(new GeometryFactory(new PrecisionModel(PrecisionModels.Floating)));
         }
+
+        private static string PointGml(string srsName) =>
+            $"<gml:Point srsName=\"{srsName}\" {GmlNamespace}><gml:pos>188473.52 193390.22</gml:pos></gml:Point>";
 
         [Theory]
         [InlineData("", false)]
@@ -26,6 +33,102 @@ namespace Be.Vlaanderen.Basisregisters.GrAr.Tests.Edit.ValidatorTests
         public void GivenGml_ThenExpectedResult(string gml, bool expectedResult)
         {
             GmlPointValidator.IsValid(gml, _gmlReader).Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(SystemReferenceId.SrsNameLambert72, SystemReferenceId.SridLambert72)]
+        [InlineData("https://www.opengis.net/def/crs/EPSG/0/31370", SystemReferenceId.SridLambert72)]
+        [InlineData(SystemReferenceId.SrsNameLambert2008, SystemReferenceId.SridLambert2008)]
+        [InlineData("https://www.opengis.net/def/crs/EPSG/0/3812", SystemReferenceId.SridLambert2008)]
+        public void GivenSupportedSrsName_ThenPointIsValidWithMatchingSrid(string srsName, int expectedSrid)
+        {
+            GmlPointValidator.IsValidPoint(PointGml(srsName), out var point).Should().BeTrue();
+
+            point.Should().NotBeNull();
+            point!.SRID.Should().Be(expectedSrid);
+            point.X.Should().Be(188473.52);
+            point.Y.Should().Be(193390.22);
+        }
+
+        [Fact]
+        public void GivenSrsNameWithDifferentCasing_ThenPointIsValid()
+        {
+            var gml = PointGml("HTTPS://WWW.OPENGIS.NET/DEF/CRS/EPSG/0/31370");
+
+            GmlPointValidator.IsValidPoint(gml, out var point).Should().BeTrue();
+
+            point.Should().NotBeNull();
+            point!.SRID.Should().Be(SystemReferenceId.SridLambert72);
+        }
+
+        [Fact]
+        public void GivenGmlWithXmlDeclaration_ThenPointIsValid()
+        {
+            var gml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + PointGml(SystemReferenceId.SrsNameLambert72);
+
+            GmlPointValidator.IsValidPoint(gml).Should().BeTrue();
+        }
+
+        [Theory]
+        // No gml
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        // Not xml
+        [InlineData("12345")]
+        [InlineData("12345 srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"")]
+        // Missing gml namespace
+        [InlineData("<gml:Point srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\"><gml:pos>188473.52 193390.22</gml:pos></gml:Point>")]
+        // Wrong gml version
+        [InlineData("<gml:Point srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.1\"><gml:pos>188473.52 193390.22</gml:pos></gml:Point>")]
+        // Missing srsName
+        [InlineData("<gml:Point xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:pos>188473.52 193390.22</gml:pos></gml:Point>")]
+        // Unsupported srsName
+        [InlineData("<gml:Point srsName=\"https://www.opengis.net/def/crs/EPSG/0/4326\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:pos>4.35 50.85</gml:pos></gml:Point>")]
+        // Not a point
+        [InlineData("<gml:Polygon srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:exterior><gml:LinearRing><gml:posList>140284.15277253836 186724.74131567031 140291.06016454101 186726.38355567306 140288.22675654292 186738.25798767805 140281.19098053873 186736.57913967967 140284.15277253836 186724.74131567031</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon>")]
+        public void GivenInvalidGml_ThenPointIsNotValidAndPointIsNull(string? gml)
+        {
+            GmlPointValidator.IsValidPoint(gml, out var point).Should().BeFalse();
+
+            point.Should().BeNull();
+        }
+
+        [Fact]
+        public void GivenSrsNameOnANestedElementOnly_ThenPointIsNotValid()
+        {
+            // Only the srsName of the outermost element is taken into account.
+            var gml =
+                "<gml:Point xmlns:gml=\"http://www.opengis.net/gml/3.2\">" +
+                "<gml:pos srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\">188473.52 193390.22</gml:pos>" +
+                "</gml:Point>";
+
+            GmlPointValidator.IsValidPoint(gml).Should().BeFalse();
+        }
+
+        [Fact]
+        public void GivenTruncatedGml_ThenPointIsValid()
+        {
+            // Documents current behaviour: the GMLReader returns the point it has already read
+            // when the document ends prematurely, so truncated gml is not rejected.
+            var gml =
+                "<gml:Point srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\">" +
+                "<gml:pos>188473.52 193390.22</gml:pos>";
+
+            GmlPointValidator.IsValidPoint(gml).Should().BeTrue();
+        }
+
+        [Fact]
+        public void GivenGmlPointWithoutPosition_ThenItThrows()
+        {
+            // Documents current behaviour: only XmlException is caught, but the GMLReader throws
+            // an ArgumentException when a gml:Point has no gml:pos.
+            var gml =
+                "<gml:Point srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"></gml:Point>";
+
+            var act = () => GmlPointValidator.IsValidPoint(gml);
+
+            act.Should().Throw<ArgumentException>();
         }
     }
 }
