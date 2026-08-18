d4f25cbb7678a865d76aede4bf522ada7aeff46f
Arne Dumarey <dumarey.arne@gmail.com>
2026-08-18 13:57:28 +0200

    fix(oslo): add gebouwgeometriemethode

---
+++ src/Be.Vlaanderen.Basisregisters.GrAr.Oslo/Gebouw/GebouwGeometrieMethode.cs
@@ -0,0 +1,23 @@
+﻿namespace Be.Vlaanderen.Basisregisters.GrAr.Oslo.Gebouw
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// De geometriemethode van het object.
+    /// </summary>
+    [DataContract(Name = "GebouwGeometrieMethode", Namespace = "")]
+    public enum GebouwGeometrieMethode
+    {
+        /// <summary>
+        /// Het object is opgemeten volgens de GRB specificaties.
+        /// </summary>
+        [EnumMember]
+        IngemetenGRB = 1,
+
+        /// <summary>
+        /// Het object is geschetst.
+        /// </summary>
+        [EnumMember]
+        Ingeschetst = 2
+    }
+}
